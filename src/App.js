a8f1c8e346cab965fd084770b0b27af46f2dda1d
Badejo Adebola <adebolabadejo11@gmail.com>
2023-10-07 17:55:23 +0100

    Added no page found message

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements ,RouterProvider,  Route} from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import "./index.css"
 import About from './components/About';
 import HomePage from './components/HomePage';
@@ -8,23 +8,25 @@ import Selector from './components/Selector';
 import Cart from './components/Cart';
 import BillingDetails from './components/BillingDetails';
 import Layout from './components/Layout';
+import NoMatch from './components/NoMatch';
 
 
-const routes =  createBrowserRouter(createRoutesFromElements(
+const routes = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Layout />}>
     <Route index element={<HomePage />} />
-    <Route path='about'  element={<About />} />
+    <Route path='about' element={<About />} />
     <Route path='contactus' element={<ContactUs />} />
     <Route path='cart' element={<Cart />} />
     <Route path='product' element={<Product />} />
+    <Route path='*' element={<NoMatch />} />
   </Route>
 ))
 
 
 function App() {
   return (
-    <RouterProvider router={routes} />  
-    )
-  }
-  
-  export default App;
+    <RouterProvider router={routes} />
+  )
+}
+
+export default App;
